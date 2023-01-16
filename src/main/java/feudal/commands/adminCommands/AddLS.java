89c85dd8cafb93474c5f51758f4cdf47f01d33e1
Alterrutm <markpav383@gmail.com>
2023-01-16 11:44:09 +0300

    я пишу в соло, смысл описывать комиты

---
+++ src/main/java/feudal/commands/adminCommands/AddLS.java
@@ -0,0 +1,23 @@
+package feudal.commands.adminCommands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class AddLS implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player) && !sender.hasPermission("feudal.op")) return false;
+
+        assert sender instanceof Player;
+        Player player = (Player) sender;
+
+
+        //add ls permission
+
+
+        return false;
+    }
+}
