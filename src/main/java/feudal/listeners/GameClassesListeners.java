b7692a10f44ae81c6d9f19d6b5acebb44a73d75a
Alterrutm <markpav383@gmail.com>
2023-01-22 17:34:58 +0300

    я пишу в соло, смысл описывать комиты

--- src/main/java/feudal/listeners/GameClassesListeners.java
+++ src/main/java/feudal/listeners/GameClassesListeners.java
@@ -9,11 +9,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 
+import static org.bukkit.Material.CROPS;
 import static org.bukkit.Material.LOG;
 
 public class GameClassesListeners implements Listener {
@@ -62,7 +66,96 @@ public class GameClassesListeners implements Listener {
             if (event.getBlock().getType().equals(LOG))
                 cachePlayerInfo.addExperience(1);
 
+        } else if (cachePlayerInfo.getAClassID() == 3) {
+
+            if (event.getBlock().getType().equals(CROPS) && event.getBlock().getMetadata("data").equals(7))
+                cachePlayerInfo.addExperience(1);
+
         }
+    }
+    @EventHandler
+    public void playerFishing(PlayerFishEvent event) {
+
+        Player player = event.getPlayer();
+
+        CachePlayerInfo cachePlayerInfo = CachePlayersHashMap.getPlayerInfo().get(player);
+
+        if (cachePlayerInfo.getAClassID() != 4) return;
+
+        if (event.getCaught() != null)
+            cachePlayerInfo.addExperience(30);
+
+    }
+    @EventHandler
+    public void entityBreed(EntityBreedEvent event) {
+
+        if (!(event.getBreeder() instanceof Player)) return;
+
+        Player player = (Player) event.getBreeder();
+
+        CachePlayerInfo cachePlayerInfo = CachePlayersHashMap.getPlayerInfo().get(player);
+
+        if (cachePlayerInfo.getAClassID() != 7) return;
+
+        switch (event.getEntityType()) {
+
+            case SHEEP:
+            case COW:
+            case MUSHROOM_COW:
+            case OCELOT:
+            case RABBIT:
+                cachePlayerInfo.addExperience(5);
+                break;
+            case PIG:
+                cachePlayerInfo.addExperience(6);
+                break;
+            case HORSE:
+            case DONKEY:
+                cachePlayerInfo.addExperience(35);
+                break;
+            case CHICKEN:
+                cachePlayerInfo.addExperience(3);
+                break;
+            case WOLF:
+            case LLAMA:
+                cachePlayerInfo.addExperience(12);
+                break;
+        }
+    }
+
+    @EventHandler
+    public void playerHunted(EntityDeathEvent event) {
+
+        Player player = event.getEntity().getKiller();
+        CachePlayerInfo cachePlayerInfo = CachePlayersHashMap.getPlayerInfo().get(player);
+
+        if (cachePlayerInfo.getAClassID() != 5) return;
+
+        switch (event.getEntityType()) {
+
+            case SHEEP:
+            case COW:
+            case MUSHROOM_COW:
+            case OCELOT:
+            case RABBIT:
+                cachePlayerInfo.addExperience(5);
+                break;
+            case PIG:
+                cachePlayerInfo.addExperience(6);
+                break;
+            case HORSE:
+            case DONKEY:
+                cachePlayerInfo.addExperience(15);
+                break;
+            case CHICKEN:
+                cachePlayerInfo.addExperience(3);
+                break;
+            case WOLF:
+            case LLAMA:
+                cachePlayerInfo.addExperience(12);
+                break;
+        }
+
     }
 
 
