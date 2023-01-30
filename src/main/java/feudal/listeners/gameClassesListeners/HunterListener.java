6a501ddcab78061fd801d071e895d43b1e5908a8
Alterrutm <markpav383@gmail.com>
2023-01-30 12:08:02 +0300

    я пишу в соло, смысл описывать комиты

--- src/main/java/feudal/listeners/gameClassesListeners/HunterListener.java
+++ src/main/java/feudal/listeners/gameClassesListeners/HunterListener.java
@@ -3,11 +3,12 @@ package feudal.listeners.gameClassesListeners;
 import feudal.databaseAndCache.CachePlayers;
 import feudal.databaseAndCache.PlayerInfo;
 import feudal.utils.enums.ClassesIDEnum;
-import org.bukkit.enchantments.Enchantment;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -25,11 +26,36 @@ public class HunterListener implements Listener {
         if (playerInfo.getAClassID() != ClassesIDEnum.HUNTER.getId() ||
                 playerInfo.getGameClassLvl() < 25) return;
 
-        if (player.getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.FIRE_ASPECT) != 0) {
 
-            if (!event.getEntity().isDead())
-                player.getInventory().getItemInMainHand().addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 1);
-            else player.getInventory().getItemInMainHand().removeEnchantment(Enchantment.FIRE_ASPECT);
+        if (event.getDrops().contains(new ItemStack(Material.RAW_CHICKEN))) {
+
+            event.getDrops().remove(new ItemStack(Material.RAW_CHICKEN));
+            event.getDrops().add(new ItemStack(Material.COOKED_CHICKEN));
+
+        } else if (event.getDrops().contains(new ItemStack(Material.RAW_BEEF))) {
+
+            event.getDrops().remove(new ItemStack(Material.RAW_BEEF));
+            event.getDrops().add(new ItemStack(Material.COOKED_BEEF));
+
+        } else if (event.getDrops().contains(new ItemStack(Material.RABBIT))) {
+
+            event.getDrops().remove(new ItemStack(Material.RABBIT));
+            event.getDrops().add(new ItemStack(Material.COOKED_RABBIT));
+
+        } else if (event.getDrops().contains(new ItemStack(Material.MUTTON))) {
+
+            event.getDrops().remove(new ItemStack(Material.MUTTON));
+            event.getDrops().add(new ItemStack(Material.COOKED_MUTTON));
+
+        } else if (event.getDrops().contains(new ItemStack(Material.PORK))) {
+
+            event.getDrops().remove(new ItemStack(Material.PORK));
+            event.getDrops().add(new ItemStack(Material.GRILLED_PORK));
+
+        } else if (event.getDrops().contains(new ItemStack(Material.RAW_FISH))) {
+
+            event.getDrops().remove(new ItemStack(Material.RAW_FISH));
+            event.getDrops().add(new ItemStack(Material.COOKED_FISH));
 
         }
 
