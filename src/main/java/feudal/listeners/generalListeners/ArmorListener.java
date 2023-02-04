a8d42b5ba75dfad843ab12bfa5354624bc231349
Alterrutm <markpav383@gmail.com>
2023-02-04 19:50:47 +0300

    я пишу в соло, смысл описывать комиты

--- src/main/java/feudal/listeners/generalListeners/ArmorListener.java
+++ src/main/java/feudal/listeners/generalListeners/ArmorListener.java
@@ -1,22 +1,30 @@
 package feudal.listeners.generalListeners;
 
 import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
-import feudal.utils.enums.ArmorEnum;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.PlayerInventory;
 import org.jetbrains.annotations.NotNull;
+
+import static feudal.utils.enums.ArmorEnum.getByItemMaterial;
 
 public class ArmorListener implements Listener {
 
     @EventHandler
     public void playerArmorChange(@NotNull PlayerArmorChangeEvent event) {
 
-        if (event.getNewItem() == null ||
-                ArmorEnum.getByItemMaterial(event.getNewItem().getType()) == 0) return;
+        if (event.getNewItem() == null) return;
 
         Player player = event.getPlayer();
+        PlayerInventory playerInv = player.getInventory();
 
-        player.setWalkSpeed(player.getWalkSpeed() - (player.getWalkSpeed() / 100 * ArmorEnum.getByItemMaterial(event.getNewItem().getType())));
+        int attribute = playerInv.getHelmet() == null ? 0 : getByItemMaterial(playerInv.getHelmet().getType());
+        attribute += playerInv.getChestplate() == null ? 0 : getByItemMaterial(playerInv.getChestplate().getType());
+        attribute += playerInv.getLeggings() == null ? 0 : getByItemMaterial(playerInv.getLeggings().getType());
+        attribute += playerInv.getBoots() == null ? 0 : getByItemMaterial(playerInv.getBoots().getType());
+
+        player.setWalkSpeed(0.2f - (0.2f / 100 * attribute));
+
     }
 }
