9d63d3daa2fd8d6c5421f71058be8eff756cb68f
Alterrutm <markpav383@gmail.com>
2023-02-06 10:31:29 +0300

    Потом опишу

--- src/main/java/feudal/utils/enums/gameClassesEnums/AttributeForGameClassesEnum.java
+++ src/main/java/feudal/utils/enums/gameClassesEnums/AttributeForGameClassesEnum.java
@@ -9,7 +9,7 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public enum AttributeForGameClassesEnum {
-    STRENGTH_AND_STAMINA(1, 3, "Сила", "Выносливость"), STAMINA_AND_LUCK(2, 8, "Выносливость", "Удача"),
+    STRENGTH_AND_STAMINA(1, "Выносливость", "Сила", 3), STAMINA_AND_LUCK(2, "Выносливость", "Удача", 8),
     SURVIVABILITY_AND_LUCK(4, "Живучесть", "Удача"), SPEED_AND_STAMINA(5, "Скорость", "Выносливость"),
     STRENGTH_AND_LUCK(6, "Сила", "Удача"), STAMINA_AND_SURVIVABILITY(7, "Выносливость", "Живучесть"),
     STRENGTH_AND_SURVIVABILITY(9, "Сила", "Живучесть");
