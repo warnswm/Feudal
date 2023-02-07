f24322eca44f66e71226c3010091842dceb34475
Alterrutm <markpav383@gmail.com>
2023-02-07 09:17:56 +0300

    потом опишу

--- src/main/java/feudal/listeners/gameClassesListeners/FishermanListener.java
+++ src/main/java/feudal/listeners/gameClassesListeners/FishermanListener.java
@@ -2,16 +2,12 @@ package feudal.listeners.gameClassesListeners;
 
 import feudal.data.builder.FeudalPlayer;
 import feudal.data.cache.CachePlayersMap;
-import feudal.utils.CreateItemUtil;
 import feudal.utils.MathUtils;
-import feudal.utils.enums.EnchantmentEnum;
+import feudal.utils.enums.gameClassesEnums.FishermanLootTableEnum;
 import feudal.utils.enums.gameClassesEnums.GameClassesIDEnum;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class FishermanListener implements Listener {
@@ -25,30 +21,11 @@ public class FishermanListener implements Listener {
                 feudalPlayer.getGameClassLvl() < 25 ||
                 event.getState() != PlayerFishEvent.State.CAUGHT_FISH) return;
 
-        int item = MathUtils.getRandomInt(1, 6), random = MathUtils.getRandomInt(1, 4);
+        int item = MathUtils.getRandomInt(1, 6), random = MathUtils.getRandomInt(1, 8);
 
         if (random != 1 && random != 2) return;
 
-        Inventory inventory = event.getPlayer().getInventory();
-
-
-        switch (item) {
-            case 1:
-                inventory.addItem((new ItemStack(Material.BOW)));
-                break;
-            case 2:
-                inventory.addItem(CreateItemUtil.createItem(Material.ENCHANTED_BOOK, EnchantmentEnum.getRandomEnchantment(), 1, 1));
-                break;
-            case 3:
-                inventory.addItem((new ItemStack(Material.FISHING_ROD)));
-                break;
-            case 4:
-                inventory.addItem((new ItemStack(Material.NAME_TAG)));
-                break;
-            case 5:
-                inventory.addItem((new ItemStack(Material.SADDLE)));
-                break;
-        }
+        event.getPlayer().getInventory().addItem(FishermanLootTableEnum.getByID(item));
 
     }
 }
---
+++ src/main/java/feudal/utils/enums/gameClassesEnums/FishermanLootTableEnum.java
@@ -0,0 +1,33 @@
+package feudal.utils.enums.gameClassesEnums;
+
+import feudal.utils.CreateItemUtil;
+import feudal.utils.enums.EnchantmentEnum;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public enum FishermanLootTableEnum {
+
+    BOW(1, new ItemStack(Material.BOW)),
+    ENCHANTED_BOOK(2, CreateItemUtil.createItem(Material.ENCHANTED_BOOK, EnchantmentEnum.getRandomEnchantment(), 1, 1)),
+    FISHING_ROD(3, new ItemStack(Material.FISHING_ROD)),
+    NAME_TAG(4, new ItemStack(Material.NAME_TAG)),
+    SADDLE(5, new ItemStack(Material.SADDLE));
+
+
+    int id;
+    ItemStack item;
+
+    public static ItemStack getByID(int id) {
+        for (FishermanLootTableEnum fishermanLootTableEnum : values())
+            if (fishermanLootTableEnum.getId() == id)
+                return fishermanLootTableEnum.item;
+        return new ItemStack(Material.STICK);
+    }
+}
