5a2bda997f38f7d53a5d019fcdada10ec9f59c60
Alterrutm <markpav383@gmail.com>
2023-02-11 11:07:40 +0300

    commit

--- src/main/java/feudal/gameClassesListeners/peasants/BuilderListener.java
+++ src/main/java/feudal/gameClassesListeners/peasants/BuilderListener.java
@@ -3,8 +3,10 @@ package feudal.gameClassesListeners.peasants;
 import feudal.data.builder.FeudalPlayer;
 import feudal.data.cache.CachePlayersMap;
 import feudal.utils.enums.gameClassesEnums.GameClassesIDEnum;
+import feudal.utils.enums.gameClassesEnums.SpawnersForBuilderEnum;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -18,12 +20,15 @@ public class BuilderListener implements Listener {
         FeudalPlayer feudalPlayer = CachePlayersMap.getFeudalPlayer(event.getPlayer());
         Block block = event.getBlock();
 
-        if (feudalPlayer.getAClassID() != GameClassesIDEnum.BUILDER.getId() &&
-                !block.getType().equals(Material.MOB_SPAWNER)) {
+        if (feudalPlayer.getAClassID() != GameClassesIDEnum.BUILDER.getId() ||
+                !block.getType().equals(Material.MOB_SPAWNER)) return;
+
+        CreatureSpawner creatureSpawner = (CreatureSpawner) block.getState();
+
+        if (!SpawnersForBuilderEnum.canBreak(creatureSpawner.getSpawnedType(), feudalPlayer.getGameClassLvl())) {
         }
 
-
-//      block.getWorld().dropItemNaturally(block.getLocation(), );
+//        block.getWorld().dropItemNaturally(block.getLocation(), );
 
     }
 }
