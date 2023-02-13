934ef7917136261c91e8faa881eb6711198d0963
Alterrutm <markpav383@gmail.com>
2023-02-13 19:37:22 +0300

    commit

--- src/main/java/feudal/utils/enums/MobsAttributesEnum.java
+++ src/main/java/feudal/utils/enums/MobsAttributesEnum.java
@@ -12,27 +12,35 @@ import org.bukkit.entity.EntityType;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public enum MobsAttributesEnum {
 
-    POLAR_BEAR(EntityType.POLAR_BEAR, 18, 60),
-    CAVE_SPIDER(EntityType.CAVE_SPIDER, 6, 24),
-    ENDERMAN(EntityType.ENDERMAN, 20, 80),
-    IRON_GOLEM(EntityType.IRON_GOLEM, 62, 200),
-    SPIDER(EntityType.SPIDER, 6, 32),
-    WOLF(EntityType.WOLF, 12, 16),
-    BLAZE(EntityType.BLAZE, 10, 40),
-    CREEPER(EntityType.CREEPER, 98, 40),
-    ELDER_GUARDIAN(EntityType.ELDER_GUARDIAN, 24, 160),
-    ENDERMITE(EntityType.ENDERMITE, 6, 16),
-    GHAST(EntityType.GHAST, 50, 20),
-    GUARDIAN(EntityType.GUARDIAN, 18, 60),
-    HUSK(EntityType.HUSK, 10, 40),
-    SHULKER(EntityType.SHULKER, 8, 60),
-    SILVERFISH(EntityType.SILVERFISH, 2, 16),
-    SKELETON(EntityType.SKELETON, 10, 40),
-    STRAY(EntityType.STRAY, 10, 40),
-    VEX(EntityType.VEX, 26, 28),
-    VINDICATOR(EntityType.VINDICATOR, 38, 48),
-    ZOMBIE(EntityType.ZOMBIE, 10, 40),
-    WITHER_SKELETON(EntityType.WITHER_SKELETON, 24, 40);
+    POLAR_BEAR(EntityType.POLAR_BEAR, 60),
+    CAVE_SPIDER(EntityType.CAVE_SPIDER, 24),
+    ENDERMAN(EntityType.ENDERMAN, 80),
+    IRON_GOLEM(EntityType.IRON_GOLEM, 200),
+    SPIDER(EntityType.SPIDER, 32),
+    WOLF(EntityType.WOLF, 16),
+    BLAZE(EntityType.BLAZE, 40),
+    CREEPER(EntityType.CREEPER, 40),
+    ENDERMITE(EntityType.ENDERMITE, 16),
+    GHAST(EntityType.GHAST, 20),
+    GUARDIAN(EntityType.GUARDIAN, 60),
+    HUSK(EntityType.HUSK, 40),
+    SHULKER(EntityType.SHULKER, 60),
+    SILVERFISH(EntityType.SILVERFISH, 16),
+    SKELETON(EntityType.SKELETON, 40),
+    STRAY(EntityType.STRAY, 40),
+    VEX(EntityType.VEX, 28),
+    VINDICATOR(EntityType.VINDICATOR, 48),
+    ZOMBIE(EntityType.ZOMBIE, 40),
+    WITHER_SKELETON(EntityType.WITHER_SKELETON, 40),
+    ELDER_GUARDIAN(EntityType.ELDER_GUARDIAN, 20, 160),
+    WITHER(EntityType.WITHER, 25, 1200),
+    ENDER_DRAGON(EntityType.ENDER_DRAGON, 30, 10000);
+
+    MobsAttributesEnum(EntityType entity, int hp) {
+        this.entity = entity;
+        this.hp = hp;
+        strength = 0;
+    }
 
     EntityType entity;
     int strength;
