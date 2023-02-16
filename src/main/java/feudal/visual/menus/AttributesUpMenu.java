29fc99324d5bfc7cb147c1bf90d2f470b6f4cfe8
Alterrutm <markpav383@gmail.com>
2023-02-16 18:09:58 +0300

    added level restriction

--- src/main/java/feudal/visual/menus/AttributesUpMenu.java
+++ src/main/java/feudal/visual/menus/AttributesUpMenu.java
@@ -12,19 +12,26 @@ public class AttributesUpMenu {
 
     public static void attributesUpMenu(Player player, int attributeLvl, String attributeName, double percent) {
 
-        Inventory strengthPumpingMenuInv = Bukkit.createInventory(player, 9, "Прокачка атрибутов");
+        Inventory attributesPumpingMenuInv = Bukkit.createInventory(player, 9, "Прокачка атрибутов");
         FeudalPlayer feudalPlayer = CacheFeudalPlayers.getFeudalPlayer(player);
 
         double percentAtt = Math.pow(1 + (percent / 100), attributeLvl) * 100;
 
+        if (attributeLvl == 100) {
+
+            attributesPumpingMenuInv.setItem(4, CreateItemUtils.createItem(Material.GRAY_SHULKER_BOX, 1, "Атрибут " + attributeName + " имеет максимальный уровень!"));
+            return;
+
+        }
+
         if (feudalPlayer.getExperience() >= percentAtt)
-            strengthPumpingMenuInv.setItem(4, CreateItemUtils.createItem(Material.GREEN_SHULKER_BOX, 1, "Прокачать уровень " + attributeName));
+            attributesPumpingMenuInv.setItem(4, CreateItemUtils.createItem(Material.GREEN_SHULKER_BOX, 1, "Прокачать уровень " + attributeName));
 
         else
-            strengthPumpingMenuInv.setItem(4, CreateItemUtils.createItem(Material.GRAY_SHULKER_BOX, 1, "Не достаточно опыта, нужно ещё " + (int) (percentAtt - feudalPlayer.getExperience())));
+            attributesPumpingMenuInv.setItem(4, CreateItemUtils.createItem(Material.GRAY_SHULKER_BOX, 1, "Не достаточно опыта, нужно ещё " + (int) (percentAtt - feudalPlayer.getExperience())));
 
 
-        player.openInventory(strengthPumpingMenuInv);
+        player.openInventory(attributesPumpingMenuInv);
 
     }
 }
