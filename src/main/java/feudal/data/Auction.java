7194b66d481a38e7e77d24d2fb2a050eb0f47e9d
Alterrutm <markpav383@gmail.com>
2023-02-21 10:04:55 +0300

    add Auction

--- src/main/java/feudal/data/Auction.java
+++ src/main/java/feudal/data/Auction.java
@@ -3,11 +3,17 @@ package feudal.data;
 import feudal.data.cache.CacheAuction;
 import feudal.utils.wrappers.ItemStackWrapper;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.experimental.FieldDefaults;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Auction {
 
@@ -16,4 +22,22 @@ public class Auction {
     final int quantityPlayers = playersProducts.size();
 
 
+    public void addPlayerProduct(@NotNull Player player, @NotNull ItemStack product, int price) {
+
+        String uuid = player.getUniqueId().toString();
+        ItemStackWrapper itemStackWrapper = new ItemStackWrapper(product.getType(), product.getDurability(), product.getAmount(), product.getItemMeta().getDisplayName(), product.getItemMeta().getLore(), product.getEnchantments(), price);
+
+        if (!playersProducts.containsKey(player.getUniqueId().toString())) {
+
+            List<ItemStackWrapper> products = new ArrayList<>();
+            products.add(itemStackWrapper);
+
+            playersProducts.put(uuid, products);
+
+            return;
+
+        }
+
+        playersProducts.get(uuid).add(itemStackWrapper);
+    }
 }
